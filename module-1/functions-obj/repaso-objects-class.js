ad88a0542a4360ff9184d567fffbcd9915836061
Miguel Garrido <hi@miguel.nz>
2023-10-07 14:55:41 +0200

    repaso-objects-class.js added

--- module-1/functions-obj/repaso-objects-class.js
+++ module-1/functions-obj/repaso-objects-class.js
@@ -1,8 +1,8 @@
 const names = ['pepe','andrea','juana','jose'];
-const cities = ['barcelona', 'roma', 'santiago', 'bilbao'];
+const cities = ['barcelona', 'roma', 'santiago', 'bilbao', 'helsinki'];
 
 /**
- * 1. Create a Class that receives an array. 
+ * 1. Create a Class that receives ONE array | class HandleWords 
  * 2. Add a method that list each element from the array in one line
  *  Eg: element 1
  *      element 2
@@ -13,3 +13,32 @@ const cities = ['barcelona', 'roma', 'santiago', 'bilbao'];
  * 
 **/
 
+class HandleWords {
+  
+  constructor(myArray) {
+    this.myArray = myArray;
+  }
+
+  storeWordsFinishingwithA() {
+    const widthA = []
+    this.myArray.forEach( item => {
+      if (item.endsWith('a')) {
+        widthA.push(item);   
+      }
+    });
+    console.log(widthA);
+  }
+
+  displayLength() {
+    console.log(this.myArray.length);
+  }
+
+}
+
+const myNames = new HandleWords(names);
+myNames.storeWordsFinishingwithA();
+myNames.displayLength();
+
+const myCities = new HandleWords(cities);
+myCities.storeWordsFinishingwithA();
+myCities.displayLength();
